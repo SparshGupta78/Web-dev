b19a8310a57f07772b2bb2421b83478ca4b5ca39
Sparsh gupta <sparshgupta78970@gmail.com>
2025-07-05 19:56:12 +0530

    Delete InputFlow directory

--- InputFlow/public/InputFlow-Form.js
+++
@@ -1,8 +0,0 @@
-    //CLEAR RESPONSE
-
-let clearResponse = (e) => {
-    let answerRadios = e.parentNode.parentNode.querySelectorAll('.answer-radios');
-    answerRadios.forEach((radio) => {
-        radio.checked = false;
-    })
-}
